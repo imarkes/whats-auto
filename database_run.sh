ee90ff939e91f6aaa552c75cebe50a67062b03df
Ivan Marques <i.markes@hotmail.com>
2024-09-17 17:53:34 -0300

    fix: evolution

--- database_run.sh
+++ database_run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 printf 'Starting container postgres '
-# docker network create --driver overlay backend &&
+docker network create --driver overlay backend &&
 docker service create --name postgres \
   -p "5432:5432" \
-  --host host=127.0.0.1 \
   --env-file .env-prod \
   -e POSTGRES_INITDB_OPTIONS='--encoding=UTF8 --locale=en_US.utf8' \
   --hostname infinitus \
