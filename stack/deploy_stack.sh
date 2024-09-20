823e6683587a5863c6401352ca419a1ede234362
Ivan Marques <i.markes@hotmail.com>
2024-09-19 21:32:59 -0300

    fix: ajusta apps

--- stack/deploy_stack.sh
+++ stack/deploy_stack.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 printf 'Starting deploy stack'
-
+# docker swarm init &&
 docker compose -f docker-swarm.yaml build &&
 echo "imagem buildada para execução" &&
-docker stack rm nginx &&
-echo "removido qualquer stack de nginx no ambiente docker" &&
-docker stack deploy --compose-file=docker-swarm.yaml nginx &&
+# docker stack rm nginx &&
+# echo "removido qualquer stack de nginx no ambiente docker" &&
+docker stack deploy --compose-file=docker-swarm.yaml app &&
 echo "realizado deploy da stack docker swarm"
